dfa64ee6a00726ee2f29ebc3d8886fc39cbc02e0
Selena Solis <selena@fathom5.co>
2020-11-17 16:41:54 -0600

    initial generated code

---
+++ db/setup.go
@@ -0,0 +1,10 @@
+package db
+import (   
+	"fmt"   
+	"github.com/globalsign/mgo"
+)
+var session *mgo.Sessionvar db *mgo.Database
+func ConnectDB() {  session, err := mgo.Dial("mongodb://localhost:27017,localhost:27018")
+if err != nil {      fmt.Println(err)   }   session.SetMode(mgo.Monotonic, true)   db = session.DB("subscriptionstest")}
+func GetCollection(collection string) *mgo.Collection {   return db.C(collection)}
+func CloseSession() {   session.Close()}
